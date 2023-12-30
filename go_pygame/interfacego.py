8d932970c7f938e49b119276783ed73f1f4529e7
Guilherme Vaz <guivaz8@gmail.com>
2023-12-30 15:24:28 +0000

    go interface

--- go_pygame/interfacego.py
+++ go_pygame/interfacego.py
@@ -9,7 +9,7 @@ pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
 
 # Define o tamanho do grid e do tabuleiro
-GRID_SIZE = SCREEN_WIDTH // 9  # Ajuste conforme necessário
+GRID_SIZE = 75  # Ajuste conforme necessário
 BOARD_SIZE = 7
 
 # Define o modo de exibição
@@ -24,43 +24,45 @@ BLACK_STONE_COLOR = (0, 0, 0)
 WHITE_STONE_COLOR = (255, 255, 255)
 BG = pygame.image.load("images/blue_background.jpg")
 
-# Calcular o deslocamento para centralizar o tabuleiro
-board_width = min(BOARD_SIZE, SCREEN_WIDTH // GRID_SIZE) * GRID_SIZE
-board_height = min(BOARD_SIZE, SCREEN_HEIGHT // GRID_SIZE) * GRID_SIZE
-board_x = 330
-board_y = 50
+
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+  return pygame.font.Font("images/font.ttf", size) 
+
 
 def draw_board(go_game):
     board_state = go_game.get_current_state()
+    board_size = len(board_state)
 
+    offset_x = (1280 - board_size * GRID_SIZE) // 2 
+    offset_y = (720 - board_size * GRID_SIZE) // 2 
     # Desenha o retângulo cinza como fundo
-    pygame.draw.rect(SCREEN, GRAY, (board_x, board_y, board_width, board_height))
 
-    for i in range(min(BOARD_SIZE, len(board_state))):
-        for j in range(min(BOARD_SIZE, len(board_state[0]))):
-            x = board_x + j * GRID_SIZE
-            y = board_y + i * GRID_SIZE
-
-            pygame.draw.rect(SCREEN, BLACK, (x, y, GRID_SIZE, GRID_SIZE), 1)
-
+    for i in range(board_size-1):
+        for j in range(board_size-1):
+            x = j * GRID_SIZE + offset_x +50
+            y = i * GRID_SIZE + offset_y +50
+            pygame.draw.rect(SCREEN, GRAY, (x, y, GRID_SIZE, GRID_SIZE), border_radius=15)
+    for i in range(board_size):
+        for j in range(board_size):
+            x = j * GRID_SIZE + offset_x +50
+            y = i * GRID_SIZE + offset_y +50
             stone = board_state[i][j]
             if stone == Go.BLACK:
                 pygame.draw.circle(SCREEN, BLACK_STONE_COLOR, (x, y), STONE_RADIUS)
             elif stone == Go.WHITE:
                 pygame.draw.circle(SCREEN, WHITE_STONE_COLOR, (x, y), STONE_RADIUS)
 
-    # Draw grid lines
-    for i in range(BOARD_SIZE + 1):
-        pygame.draw.line(SCREEN, BLACK, (board_x + i * GRID_SIZE, board_y),
-                         (board_x + i * GRID_SIZE, board_y + board_height), 2)
-        pygame.draw.line(SCREEN, BLACK, (board_x, board_y + i * GRID_SIZE),
-                         (board_x + board_width, board_y + i * GRID_SIZE), 2)
 
 def play_go(board_size):
     go_game = Go(small_board=True)
     player = 1
 
     while True:
+        SCREEN.blit(BG,(0,0))
+        Ataxx_MENU_TEXT = get_font(50).render("GO", True, "#d7fcd4")
+        Ataxx_MENU_RECT = Ataxx_MENU_TEXT.get_rect(center=(180,100))
+        SCREEN.blit(Ataxx_MENU_TEXT, Ataxx_MENU_RECT)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -68,15 +70,18 @@ def play_go(board_size):
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                col = (mouse_pos[0] - board_x) // GRID_SIZE
-                row = (mouse_pos[1] - board_y) // GRID_SIZE
+                col = (mouse_pos[0] - ((1280 - 400) // 2)) // 100 +1
+                row = (mouse_pos[1] - ((720 - 400) // 2)) // 100 +1
 
-                if go_game.is_valid_move(go_game.get_current_state(), (row, col), player):
+                print(col, row)
+                print(go_game.get_current_state())
+                if go_game.is_valid_move(go_game.get_current_state(), (row, col), player) and go_game.get_current_state()[row][col] == 0 and row >=0 and col >=0 :
+                    print("move")
                     go_game.make_move((row, col), player)
                     player = -player  # Switch player after a move
-
+        x, y, player = hover_to_select(player, valid_moves, click)
         draw_board(go_game)
         pygame.display.update()
 
 if __name__ == "__main__":
-    play_go(7)  # Você pode ajustar o tamanho do tabuleiro conforme necessário
+    play_go(9)  # Você pode ajustar o tamanho do tabuleiro conforme necessário
